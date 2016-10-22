99ae7472f85c85752492783333719e3a8211be71
macbook <macbook@macbooks-MacBook-Air.local>
2016-10-22 23:38:37 +0530

    init

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
+
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+	$('#about').click(function(){
+		$('html, body').animate({scrollBottom : 800},0);
+		return false;
+	});
+	
+});
+
